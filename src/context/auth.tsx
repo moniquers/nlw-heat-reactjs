0d407a4e48e660a10a992d4433947b7be926b196
Monique Ribeiro Sousa <ribeiromonique90@gmail.com>
2021-10-22 18:07:47 -0300

    implements auth

---
+++ src/context/auth.tsx
@@ -0,0 +1,86 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+    id: string;
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+    signOut: () => void;
+}
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+};
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider(props: AuthProvider) {
+
+    const [user, setUser] = useState<User | null>(null);
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=afd7f0fb304b7a00bb4a`
+
+    async function signIn(githubCode: string) {
+        const response = await api.post<AuthResponse>("authenticate", {
+            code: githubCode
+        });
+
+        const { token, user } = response.data;
+
+        localStorage.setItem("@dowhile:token", token);
+
+        setUser(user);
+
+    }
+
+    function signOut() {
+        setUser(null);
+        localStorage.removeItem('@dowhile:token');
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@dowhile:token");
+        api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+        if (token) {
+            api.get<User>("profile").then(response => {
+                setUser(response.data);
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes("?code=");
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=');
+
+            window.history.pushState({}, '', urlWithoutCode);
+
+            signIn(githubCode);
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/globals.css'
 import { App } from './App'
+import { AuthProvider } from './context/auth'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
